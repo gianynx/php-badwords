54015a6eb7a823f7da25368e6afe08d24fe7aeca
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-03 20:23:39 +0200

    modify second_page.php

--- index.php
+++ index.php
@@ -5,14 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <title>PHP Badwords</title>
 </head>
 <body>
+<header>
+    <h1 class="text-center pt-4">Enter a text and the word to censor!</h1>
+</header>
 
 <form action="second_page.php" method="POST" class="pt-5 mx-5">
     <div class="form-floating">
         <textarea class="form-control" placeholder="Leave a comment here" name="text" id="text" style="height: 100px"></textarea>
         <label for="text">enter your text!</label>
+    </div>
+
+    <div class="mt-5">
+        <label for="text">enter the word to be censored!</label>
+        <input class="form-control" name='word'>
     </div>
 
     <input type="submit" value="click me!" class="mt-3">
--- second_page.php
+++ second_page.php
@@ -1,5 +1,10 @@
 <?php
-$paragraph = $_POST['text'];
+$length = 0;
+if (isset($_POST['text'])) {
+    $paragraph = trim($_POST['text']);
+    $word = trim($_POST['word']);
+    $length = strlen($paragraph);
+}
 ?>
 
 
@@ -9,11 +14,23 @@ $paragraph = $_POST['text'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <title>Second Page</title>
 </head>
 <body>
 <h1>
-    <?php echo $paragraph ?>
+    <p>
+        <?php echo $paragraph ?>
+        <span>text length: <?php echo $length ?></span>
+    </p>
+    <p>
+        <?php  echo str_replace($word, '***', $paragraph) ?>
+        <span>text length: <?php echo $length ?></span>
+    </p>
 </h1>
     
 </body>
