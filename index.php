3ddb8e8d1a41850a06abf1185287dcc5a5a27d35
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-03 18:05:58 +0200

    add bootstrap

--- index.php
+++ index.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
 
-<form action="second_page.php" method="POST">
-    <label for="text">Enter your text!</label>
-    <div>
-        <textarea name="text" id="text" cols="30" rows="10"></textarea>
+<form action="second_page.php" method="POST" class="pt-5 mx-5">
+    <div class="form-floating">
+        <textarea class="form-control" placeholder="Leave a comment here" name="text" id="text" style="height: 100px"></textarea>
+        <label for="text">enter your text!</label>
     </div>
 
-    <input type="submit" value="Click me!">
+    <input type="submit" value="click me!" class="mt-3">
 </form>
     
 </body>
